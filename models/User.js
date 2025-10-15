408f80f538d945a4591cd67cce7a2758ab0a7813
ghamzakha20-coder <ghamzakhan20@gmail.com>
2025-10-15 15:58:50 +0500

    Pushing QuickCart-App

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mognoose.Schema({
+    _id: { type: String, reguired:true },
+    name: { type: String, reguired:true },
+    email: { type: String, reguired:true, unique:true },
+    imageUrl: { type: String, reguired:true },
+    cartItem: { type: Object, default: {} }
+}, { minimize: false })
+
+const User = mongoose.models.user || mongoose.model('user', userSchema)
+
+export default User
